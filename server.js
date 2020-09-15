b771130e715880e77ab8f3ceea97bcc8903e0492
deniskud <denis.kudriakov@gmail.com>
2020-09-15 11:38:07 +0300

    multi out add wo infinity

--- server.js
+++ server.js
@@ -10,21 +10,22 @@ const SERVERHOST='0.0.0.0';
 //var HOST='192.162.132.124'; // pumps
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
-
-
-var client  = new Object;
-client={ptr: dgram.createSocket('udp4'),num: 0};
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+const infostream =  new MyEmitter();
+myEmitter.on('cmd', (cmd) => {
+  console.log('an event occurred!');
+});  ////myEmitter.emit('cmd');
+var stopflag=new Array;
 var clients = new Array;
-clients[0]= client;//open for future. time-economy
-
-
-//var clients[0] = dgram.createSocket('udp4'); 
+var newclient  = new Object;
+newclient={ptr: dgram.createSocket('udp4'),num: 0,cnt:0,ptrn:0};
+clients[0]= newclient;//open for future. time-economy
 const fs = require("fs");
-
 ///////////////////////my variables
-//cliets
 var indtcnt =0;
-var flag2stop =0 ;
+var flag2stop = 0 ;
 var statemove_el = 0;
 var statemove_az = 0;
 //variable - flag-status brake-stat 0-off 1-brake-on
@@ -35,7 +36,6 @@ var  AZ_SOFTLIMIT_CW   = 10; // 180
 var  AZ_SOFTLIMIT_CCW  = 20; // 0
 var  EL_SOFTLIMIT_UP   = 30; // 88
 var  EL_SOFTLIMIT_DOWN = 40; // 5
-
 var  AZ_OFFSET = 0;   //  <---+______TODO: initfunction. 
 var  EL_OFFSET = 0;   //  <---+
 //END TODO---------------------------
@@ -57,23 +57,6 @@ function hexdump(msg){  //return string in
   }
   return tmpstr;
 }
-function num2hex1(num){
-  if (num<255) return String.fromCharCode(num); 
-  else return -1;
-}
-function div2rad(mega){ 
-  if (mega>=0 && mega<=1048576) return mega*Math.PI*2/1048576;
-  else return -1;
-}
-function rad2div(ang){ 
-  if (ang==0) return 0;
-  else return Math.round((pi2/ang)*1048576);
-}
-function div2rad_neg(mega){ 
-  if (mega>524288 || mega<-524288) consolelog("! error value: "+mega);
-  else  return ((mega*pi2/1048576)-pina2);
-}
-function rad2div_neg(ang){return (rad2div(ang+pina2)-524288);}
 ////////////////////////////////////////////////
 dgsize=[9,5,10,12,10,12,6,5,10,12,10,12,6,5,20];
 ////////////////////////////////////////////////
@@ -83,7 +66,7 @@ function validation(message){
 //detection command number. valid range 0..10
   var cmd=message[2];
   if (cmd<0 || cmd>15) return 3; //unknown command
-  if (dgsize[cmd]!=message.length) return 4;
+  if (dgsize[cmd]!=message.length) return 4; //coomand size
 // validation code:
 // good data           0
 // bad args            1
@@ -94,49 +77,55 @@ function validation(message){
 }
 ///////////////////////////////////////////////////////////////////////
 function getxy(){return Math.sqrt(ts.getTime);};
-function createinfo(max,port,num,pattern){
-consolelog("trying create streem @ max : "+ max +
-  " port: "+ port +
-  " numb: "+ number +
-  " ptrn: "+ pattern );
-  for (var i=0;i<clients.length;i++) if (clients.num==num) return 1; 
-  clients[0].num=num;
-  
 
-  clients[clients.length]=clients[0];
-  for (var i=0;i<max;i++){ 
-    otmsg=new Buffer.from("123456789012345678");//size=18
-    outmsg[0]= 0x7e;
-    outmsg[1]= 0x7e;
-    outmsg[2]= 0x7e;
-    outmsg[3]= 0x7e;
-    outmsg[4]= 0x7e;
-    outmsg[5]= 0x7e;
-    outmsg[6]= 0x7e;
-    outmsg[17]= 0x7f;
-
-    clients[0].ptr.send(outmsg,0,outmsg.legth,port);
-  }
-  /*
-    server.send(packetResponse, 0, packetResponse.length, remote.port, 
-  remote.address, function(err, bytes) {
-    if (err) throw err;
-    consolelog('> snt UDP server message response to ' + 
-      remote.address + ':' + 
-      remote.port +' [' + 
-      hexdump(packetResponse) + ']');
-    consolelog('____________');
-  });
-
-  */
-  
+function createinfo(max,port,num,pattern,address) {
+  consolelog("trying create streem @ max : "+ max + " port: "+ port +
+    " numb: "+ num + " ptrn: "+ pattern );
+  var i=0;
+  for (i=0;i<clients.length;i++) if (clients[i].num==num) {
+    consolelog("! stream #"+num+" allredy present!");
+    return 1
+  }; 
+  clients[0].num=num; 
+  var lastidx=clients.length;
+  clients[lastidx]=clients[0]; //create new array element
+  var msgbuf=new Buffer.from("\x7f"+"1234"+"\x7e");//size=6    
+  var cnt=0;
+  if (max) cnt=0; else cnt=-1;  
+  var p=new Array;
+  while (cnt<max){
+    p[i] = new Promise(function(resolve,reject){
+      var outmsg=new Buffer.from("\x7f"+"1234"+"\x7e");//size=6    
+      outmsg[0] = 0x7e;
+      outmsg[1] = (i &0xff000000)>>24;
+      outmsg[2] = (i &0xff0000)>>16;
+      outmsg[3] = (i &0xff00)>>8;
+      outmsg[4] = (i &0xff);
+      outmsg[outmsg.length-1] = 0x7f;        
+//      console.log("---- n="+i+" "+ hexdump(outmsg));
+      resolve(outmsg);
+      reject("reject");
+    })
+    p[i].then((fulfilled) => {
+      clients[lastidx].ptr.send(fulfilled,0,fulfilled.length,port,) 
+      clients[lastidx].cnt++; 
+    });
+    if (max) cnt++;
+    i++;
+    if (stopflag[num]) cnt=max+1; 
+  }; // end loop
+  consolelog(">> sendeded" +clients[lastidx].cnt + " dgrams @N:"+ clients[lastidx].num +" ");
+  clients[lastidx].num=clients[lastidx].ptrn=clients[lastidx].cnt=0; //clear obj
+  clients[0]= {ptr: dgram.createSocket('udp4'),num: 0,cnt: 0,ptrn: 0};   
   return 0;    
-}
+}//end function createinfo()
 
-function startcommand(msg){
+function startcommand(msg,sender){
   if (msg[2]==0) {
-    if (!createinfo(msg[4],(msg[6]+(msg[5]<<8)),msg[3],msg[7])) 
+    if (!createinfo(msg[4],(msg[6]+(msg[5]<<8)),msg[3],msg[7],sender.address)){ 
     consolelog('* command '+ msg[2]+' strted');
+    }
+    else consolelog("! error create stream!");
   }
   else consolelog(
   '* command of movement temporary not available ');
@@ -167,7 +156,6 @@ server.on('message', function (message, remote) {
   packetResponse[2]=message[2]; 
   packetResponse[3]=validstatus;
   packetResponse[4]=0x7f;
-
   if (validstatus==1) msglog=("! Error packet args ["+ 
     hexdump(message) +"]");        //bad argument
   else if (validstatus==2) msglog=("! error packet format" );//bad in packet
@@ -179,7 +167,8 @@ server.on('message', function (message, remote) {
     "]" + " packet N=" +dtnum  );        //bad incoming packet
   else if (validstatus==0)  {
     msglog=('* CMD Ok [' + command + ']' + " packet N=" +dtnum  ); //pck&arg Ok!   
-    startcommand(message);    //                 <--------------------starter
+    startcommand(message,remote);    //   Synhro              <--------------------starter
+// myEmitter.emit('cmd');     // asynchro
   };
   consolelog(msglog +' from ' + remote.address + ':' + remote.port);
   /////
@@ -193,9 +182,7 @@ server.on('message', function (message, remote) {
     consolelog('____________');
   });
   ////////////  
-});
+});//on.message
 // main: //////////////////////////////////////////
-server.bind(SERVERPORT, SERVERHOST, function(){
-//  consolelog('- Server binded @'+SERVERHOST+':'+SERVERPORT);  
-});
+server.bind(SERVERPORT, SERVERHOST, function(){});
 
--- testcliet.js
+++ testcliet.js
@@ -1,7 +1,19 @@
 var ver='200906-01'
+var debug=1;
+function consolelog(msg){
+  if (debug) {
+    ts = new Date();
+    var textlog=(ts.getTime()+'. ' + msg);
+    console.log(textlog);
+    if (debug>2) textlog=textlog+' <br>';
+    if (debug>1) fs.appendFileSync("./server.log", textlog+'\n');
+  }
+}
 
 var OPU_SERVER_PORT = 9090; // PORT -- 4 connect 2 OPU-SERVER
 var OPU_SERVER_HOST='127.0.0.1';//  PORT -- 4 connect 2 OPU-SERVER
+//var OPU_SERVER_HOST='178.158.224.123'; //  PORT -- 4 connect 2 OPU-SERVER
+
 
 var MY_SERVER_PORT  = 7777; // my PORT -- 4 connect from OPU-CLIENT 2 MYSERVER 
 var MY_SERVER_HOST='0.0.0.0'; // 0 - all interfaces
@@ -14,7 +26,7 @@ var ts = new Date();
 
 var packn = 1;
 var cmd = 0; // 0..11
-var max = 100;
+var max = 255;
 var pattern = 1;
 var tmp=0;
 
@@ -27,46 +39,60 @@ function hexdump(msg){
   return tmpstr;
 }
 
-function sendcommand0(){
-  var message = new Buffer.from('123456789')
-  message[0]=0x7e;         // start byte
-  message[1]=packn & 0xff; // packet number
-  message[2]=0;            // cmd
-  message[3]=77;           //stream number max
-  message[4]=max;          //max
-  message[5]=(MY_SERVER_PORT & 0xFF00)>>8;  // big 
-  message[6]=MY_SERVER_PORT & 0xFF;         // little
-  message[7]=1;            // patern
-  message[8]=0x7f;         // stop byte
+/////// events /////
+myserver.on('error', function (err){
+  consolelog('! server error: '+err.stack);
+  server.close(function(){consolelog('!!!!!!!!!!!server down!!!!!!!!!!!!');});
+});
 
-  myserver.on('error', function (err){
-    consolelog('! server error: '+err.stack);
-    server.close(function(){console.log('!!!!!!!!!!!server down!!!!!!!!!!!!');});
+myserver.on('listening', function () {
+  var address = myserver.address();
+  consolelog('* Start UDP Server listening on ' + 
+  address.address + ":" + 
+  address.port);
+});
+
+myserver.on('message', function (message, remote){
+  tmp++;
+  if (tmp<=max) {
+    consolelog("tmp="+(tmp)+' n='+((message[1]<<8)+message[2])+" "+ hexdump(message));
+  } 
+  if (tmp>=max) myserver.close(()=>{consolelog(tmp+" datagrams recived");});
+});
+
+
+myclient.on('message', function (message, remote) {
+  ts = new Date();
+  consolelog('< RCV '+remote.address + ':' + remote.port +' - [' + hexdump(message)+']');
+  myclient.close();
+});
+
+/////////end events //////////
+var message = new Buffer.from('123456789') 
+message[0]=0x7e;         // start byte
+message[1]=packn & 0xff; // packet number
+message[2]=0;            // cmd
+message[3]=7;           //stream number max
+message[4]=max;          //max
+message[5]=(MY_SERVER_PORT & 0xFF00)>>8;  // big 
+message[6]= MY_SERVER_PORT & 0xFF;         // little
+message[7]=1;            // patern
+message[8]=0x7f;         // stop byte
+// bind serever//////////////////////////////////////////////////////
+myserver.bind(MY_SERVER_PORT, MY_SERVER_HOST, function(){
+  //after binding:
+  var address = myserver.address();    
+  consolelog('# Server binded @'+address.address+':'+address.port);
+  //sending message:
+  myclient.send(message, 0, 9, OPU_SERVER_PORT, OPU_SERVER_HOST, function(err, bytes) {
+    //after sending message:
+    if (err) throw err;
+    ts = new Date();
+    consolelog('* Start command. Open new infostreeam ');
+    consolelog('> SND UDP client message [' +hexdump(message)+ '] sent to ' + OPU_SERVER_HOST +':'+ OPU_SERVER_PORT);
   });
+  //
+});
+/*  */
+///////////////////////////////
 
-  myserver.on('listening', function () {
-    var address = myserver.address();
-    console.log('* Start UDP Server listening on ' + 
-    address.address + ":" + 
-    address.port);
-  });
-  myserver.on('message', function (message, remote){console.log((tmp++)+' '+ hexdump(message));});
-
-  myserver.bind(MY_SERVER_PORT, MY_SERVER_HOST, function(){
-    console.log('- Server binded @'+myserver.address.address+':'+myserver.address.port);
-    myclient.send(message, 0, 9, OPU_SERVER_PORT, OPU_SERVER_HOST, function(err, bytes) {
-      if (err) throw err;
-      ts = new Date();
-      console.log(ts.getTime()+' Start command. Open new infostreeam ');
-      console.log(ts.getTime()+' SND UDP client message [' +hexdump(message)+ '] sent to ' + OPU_SERVER_HOST +':'+ OPU_SERVER_PORT);
-      myclient.on('message', function (message, remote) {
-        ts = new Date();
-        console.log(ts.getTime()+' RCV '+remote.address + ':' + remote.port +' - [' + hexdump(message)+']');
-        myclient.close();
-      });
-    });
-  });
-
-} // end function sendcommand0
-
-sendcommand0(function (){console.log('The end!');});
